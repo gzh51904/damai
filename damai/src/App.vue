6592b338b8bf1e93f00ea48a6e95a89583ed5b2e
黎剑锋 <510194165@qq.com>
2019-07-13 14:10:01 +0800

    保佑

--- damai/src/App.vue
+++ damai/src/App.vue
@@ -1,7 +1,4 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
 <div id="app">
     <!-- <mt-header fixed title="现在是年轻人的世界"></mt-header> -->
 
@@ -9,19 +6,7 @@
     <!-- <h1>app.vue页面</h1> -->
 </div>
 </template>
-<script>
 
-export default {
+<style scoped>
 
-};
-</script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-}
-#app{
-  background: white;
-}
 </style>
